aaf80c8cec359509de82f32266a69847c3ab0366
yorkin0611 <159424998+yorkin0611@users.noreply.github.com>
2024-02-08 09:21:59 +0000

    Add files via upload

---
+++ second_pythonfile.py
@@ -0,0 +1,11 @@
+"""Enter two numbers. Print operations and results of 
+the summation, subtraction, multiplication and division."""
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} * {b} = {a * b}")
+print(f"{a} / {b} = {a / b}")
+print(f"{a} // {b} = {a // b}")
